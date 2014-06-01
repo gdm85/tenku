29bacde903177c28b68fad27d4455100f2d0e873
gdm85 <gdm85@users.noreply.github.com>
2014-06-02 00:52:55 +0200

    * adding scripts for creation/spawning of gitian hosts

---
+++ docker/scripts/create-gitian-host.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ../gitian-host || exit $?
+
+if [ ! -f authorized_keys ]; then
+	echo "No authorized_keys file found in $PWD"
+	if [ -f ~/.ssh/id_rsa.pub ]; then
+		echo "Do you want to use ~/.ssh/id_rsa.pub? (y/n)"
+		read -r ANSWER
+		if [[ "$ANSWER" == "y" ]]; then
+			cp -v ~/.ssh/id_rsa.pub authorized_keys || exit $?
+		else
+			exit 1
+		fi
+	else
+		exit 1
+	fi
+fi
+
+docker build --tag=gdm85/gitian-host . && \
+echo "Gitian host image created successfully!" && \
+echo "You can now spawn containers with spawn-gitian-host.sh"
