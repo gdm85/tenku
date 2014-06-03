d2ba944cd862b728d815bcc4e2add1dc654f8881
gdm85 <gdm85@users.noreply.github.com>
2014-06-03 13:22:06 +0200

    * added support for newer versions of bitcoin

---
+++ docker/gitian-bitcoin-host/sign.sh
@@ -0,0 +1,13 @@
+-#!/bin/bash
+set -e
+
+if [[ ! $# -eq 2 ]]; then
+	echo "Please specify version and signer id" 1>&2
+	exit 1
+fi
+
+VERSION="$1"
+SIGNER="$2"
+
+cd gitian-builder
+./bin/gsign --signer $SIGNER --release ${VERSION} --destination ../gitian.sigs/ ../bitcoin/contrib/gitian-descriptors/gitian-linux.yml
