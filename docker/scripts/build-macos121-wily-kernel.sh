7a92ae88586cc0c8e3d94ca417aea9368d178994
gdm85 <gdm85@users.noreply.github.com>
2015-11-24 10:58:13 +0100

    Most recent version (with module parameters) Fixed an issue with inspecting a base image

--- docker/scripts/build-macos121-wily-kernel.sh
+++ docker/scripts/build-macos121-wily-kernel.sh
@@ -24,30 +24,6 @@ make wily
 
 cd ../ubuntu-kernelbuilder
 
-cat<<EOF > patches/macos-121-btusb-fix.patch
---- a/drivers/bluetooth/btusb.c 2015-10-28 14:52:20.466644715 +0000
-+++ b/drivers/bluetooth/btusb.c 2015-10-28 14:52:34.715107143 +0000
-@@ -2657,7 +2657,7 @@
- 	BT_DBG("intf %p id %p", intf, id);
- 
- 	/* interface numbers are hardcoded in the spec */
--	if (intf->cur_altsetting->desc.bInterfaceNumber != 0)
-+	if (intf->cur_altsetting->desc.bInterfaceNumber != 2)
- 		return -ENODEV;
- 
- 	if (!id->driver_info) {
-@@ -2827,7 +2827,7 @@
- 		data->isoc = NULL;
- 	} else {
- 		/* Interface numbers are hardcoded in the specification */
--		data->isoc = usb_ifnum_to_if(data->udev, 1);
-+		data->isoc = usb_ifnum_to_if(data->udev, 3);
- 	}
- 
- 	if (!reset)
-
-EOF
-
 make wily linux-image-wily
 
 echo "Linux kernel .deb packages are now available in packages/"
